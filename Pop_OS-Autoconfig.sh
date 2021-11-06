5b1b69570a67ecaf6a7e46a027ce0c8fa44f93be
Anaximandro Souza <anaxec1@gmail.com>
2021-11-06 16:19:09 -0300

    Update Pop_OS-Autoconfig.sh

--- Pop_OS-Autoconfig.sh
+++ Pop_OS-Autoconfig.sh
@@ -61,7 +61,15 @@ flatpak install flathub com.discordapp.Discord -y #Discord
 sudo apt install software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add –
 sudo add-apt-repository “deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main”
+sudo apt-get update
 sudo apt install code
+
+## Instalando Google Chrome ##
+
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
 
 # ---------------------------------------------------------------------- #
 
